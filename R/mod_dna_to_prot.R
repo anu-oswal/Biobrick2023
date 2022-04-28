cee9423e76ab8612293a031b3b432e42bf8d439c
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-28 13:40:41 +0000

    Attempted to deploy app, but failed

--- R/mod_dna_to_prot.R
+++ R/mod_dna_to_prot.R
@@ -11,10 +11,23 @@ mod_dna_to_prot_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(8, "DNA_sequence"),
-      column(4, "random_dna_length", "generate_dna_button")
+      column(8, shiny::uiOutput(ns("DNA"))),
+      column(4, shiny::numericInput(
+        inputId = ns("dna_length"),
+        value = 6000,
+        min = 3,
+        max = 100000,
+        step = 3,
+        label = "Random DNA length"
+      ),
+      shiny::actionButton(
+        inputId = ns("generate_dna"),
+        label = "Generate random DNA", style = "margin-top: 18px;"
+      ))
     ),
-    "peptide_sequence"
+    shiny::verbatimTextOutput(outputId = ns("peptide")) %>%
+      shiny::tagAppendAttributes(style = "white-space: pre-wrap;")
+
   )
 }
 
@@ -23,8 +36,37 @@ mod_dna_to_prot_ui <- function(id){
 #' @noRd
 mod_dna_to_prot_server <- function(id){
   moduleServer( id, function(input, output, session){
+    dna <- reactiveVal()
     ns <- session$ns
-
+    output$DNA <- renderUI({
+      textAreaInput(
+        inputId = ns("DNA"),
+        label = "DNA sequence",
+        placeholder = "Insert DNA sequence",
+        value = dna(),
+        height = 100,
+        width = 600
+      )
+    })
+    observeEvent(input$generate_dna, {
+      dna(
+        centralDogma::random_dna(input$dna_length)
+      )
+    })
+    output$peptide <- renderText({
+      # Ensure input is not NULL and is longer than 2 characters
+      if(is.null(input$DNA)){
+        NULL
+      } else if(nchar(input$DNA) < 3){
+        NULL
+      } else{
+        input$DNA %>%
+          toupper() %>%
+          centralDogma::transcribe() %>%
+          centralDogma::codon_split() %>%
+          centralDogma::translate()
+      }
+    })
   })
 }
 
